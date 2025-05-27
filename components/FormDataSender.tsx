c1cbfa48b9698d400724bb749d847d147363d30f
Avraham-prog <avi@n-a.co.il>
2025-05-27 14:10:00 +0300

    Update FormDataSender.tsx

--- components/FormDataSender.tsx
+++ components/FormDataSender.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { Input } from "./ui/input";
-import { Textarea } from "./ui/textarea";
-import { Button } from "./ui/button";
-import ChatSidebar from "./ChatSidebar";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import ChatSidebar from "@/components/ChatSidebar";
 import axios from "axios";
 
 interface Message {
