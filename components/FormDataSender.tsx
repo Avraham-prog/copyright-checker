8301afae791f66c0492745e568b5f7e4b70c1af1
Avraham-prog <avi@n-a.co.il>
2025-05-27 14:16:55 +0300

    Update FormDataSender.tsx

--- components/FormDataSender.tsx
+++ components/FormDataSender.tsx
@@ -138,7 +138,13 @@ export default function FormDataSender() {
 
   return (
     <div className="flex h-screen">
-      <ChatSidebar />
+      <ChatSidebar
+  chats={chats}
+  currentChatId={currentChatId}
+  onSelect={handleSelectChat}
+  onDelete={handleDeleteChat}
+  onNewChat={handleNewChat}
+/>
       <div className="flex flex-col flex-1 h-screen max-w-4xl mx-auto border rounded shadow bg-white overflow-hidden">
         <div className="flex-1 overflow-y-auto p-4 space-y-4">
           {messages.map((msg, index) => (
